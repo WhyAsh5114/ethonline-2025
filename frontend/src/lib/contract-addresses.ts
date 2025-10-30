a1230800e029ad53efe39b126d3e904daeb9f254
WhyAsh5114 <whyash5114@gmail.com>
2025-10-30 13:11:56 +0000

    feat: add option to fund while deployment

--- frontend/src/lib/contract-addresses.ts
+++ frontend/src/lib/contract-addresses.ts
@@ -9,7 +9,7 @@ export const CONTRACT_ADDRESSES = {
   // Sepolia Testnet
   sepolia: {
     entryPoint: "0x0000000071727De22E5E9d8BAf0edAc6f37da032",
-    verifier: "0xd3961aeac5f456a7efa3751a072a9ae5748c754b",
+    verifier: "0xed0a886d6fe34805048541de4b9ac70ce259f113",
   },
   // Hardhat Local
   hardhat: {
